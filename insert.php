11beab04ecda571a1bea3b2aad227a4cd4a6c41b
RicoInguito <ricoinguito5@gmail.com>
2023-05-13 12:57:37 +0800

    first code

---
+++ insert.php
@@ -0,0 +1,18 @@
+<?php
+  abstract class people
+  {
+    public $conn;
+    public $servername = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $dbname = "abstract";
+
+    public function __construct()
+    {
+        $this->conn = new mysqli($this->servernam, $this->username, $this->password);
+        $dis=("CREATE DATABASE IF NOT EXISTS")
+        $this->conn->query($dis);
+    }
+  }
+
+?>
